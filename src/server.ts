2308cd5826c26ec1e2eb84f6b9416d71f26d79ac
Davi Silva <davi.silva@code7.com>
2021-04-19 10:46:44 -0300

    feat(connection): updates port
    updates ports

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
   console.log(`[OK] ${process.env.APP_NAME} is listening on port: ${port}`);
